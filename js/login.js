29198dfb3140c51cda1273962fcf81537b260e59
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-02-03 15:04:12 +0900

    로그인, 비밀번호 입력창 비었을경우 경고창

--- js/login.js
+++ js/login.js
@@ -5,8 +5,16 @@ $(document).ready(function(){
             Id : $("#inputLoginId").val(),
             Pw : $("#inputLoginPw").val(),
             is_ajax : 1
-            //is_ajax의 의미
         };
+
+        if(form_data.Id==''){
+            alert("아이디가 입력되지 않았습니다.");
+            return false;
+        } 
+        if(form_data.Pw==''){
+            alert("비밀번호가 입력되지 않았습니다.");
+            return false;
+        }
 
         var html = `<p class="alert_text">로그인성공.</p><input type="submit" value="완료" id="loginSuccess">`
         $.ajax({
@@ -15,13 +23,10 @@ $(document).ready(function(){
             data : form_data,
             dataType : 'json',
             success : function(result){
-                console.log(result)
                 var data = result;
                 if(result.success){
-                    console.log(result);
                     $("#message").html(html).data(result);
                     $("#message").show();
-                    // $("#loginform").slideUp('slow');
 
                     $("input[name=loginUserId]").val(result.loginUserId);
                     $("input[name=name]").val(result.name);
@@ -29,22 +34,17 @@ $(document).ready(function(){
                     return;
                 }
                 else{
-                    console.log(result);
-                    // $("#message").html("<p class='error'>아이디 또는 비밀번호가 잘못되었습니다.</p>");
-                    // $("#message").show();
                     alert('아이디 또는 비밀번호가 잘못되었습니다.');
                 }
             }
         });
         
         return false;
-        //무슨의미?
     });
     
     $(document).on("click", "#loginSuccess", function(){
-        alert("hello");
-        var data = $("#message").data()
-        // $.post( "board_login.php", data).done(function( data ) { $("body").data(data);});
+        var data = $("#message").data();
+        alert("Hello " + data.loginUserId);
         window.location = 'board_list.php'
     })
 
